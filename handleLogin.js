afccc1d9cba1b6c907174a4ca1b2b1e68614c358
Ravin Sardal <ravin.sardal@gmail.com>
2014-10-04 12:55:01 -0700

    Intercepting submits from login forms

--- handleLogin.js
+++ handleLogin.js
@@ -1,2 +1,13 @@
-console.log(chrome.runtime.id);
-chrome.runtime.sendMessage("HELLYEAH");
+var i;
+var form;
+var onsubmit;
+
+for (i = 0; i < document.forms.length; i++) {
+	form = document.forms[i];
+	onsubmit = form.onsubmit;
+	form.onsubmit = function () {
+		if (onsubmit) onsubmit();
+
+		chrome.runtime.sendMessage("HELLYEAH");
+	};
+}
