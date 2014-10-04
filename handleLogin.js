35958506529c8eba8669dce97071475bd89aad8f
Simon Deng <simonsdeng@gmail.com>
2014-10-04 13:57:26 -0700

    Add form capturing

--- handleLogin.js
+++ handleLogin.js
@@ -1,13 +1,24 @@
-var i;
+var i, j;
 var form;
 var onsubmit;
 
-for (i = 0; i < document.forms.length; i++) {
-	form = document.forms[i];
-	onsubmit = form.onsubmit;
-	form.onsubmit = function () {
+var generateHandler = function (form, i, onsubmit) {
+	return function () {
 		if (onsubmit) onsubmit();
 
-		chrome.runtime.sendMessage("HELLYEAH");
+		var data = {};
+		var input;
+
+		for (j = 0; j < form.elements.length; j++) {
+			input = form.elements[j]
+			if (input.name) data[input.name] = input.value;
+		}
+
+		chrome.runtime.sendMessage({form: i, data: data});
 	};
+};
+
+for (i = 0; i < document.forms.length; i++) {
+	form = document.forms[i];
+	form.onsubmit = generateHandler(form, i, form.onsubmit);
 }
