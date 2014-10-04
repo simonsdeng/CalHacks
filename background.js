bf105da7fe31fc63eab302daf8722072edc99c02
Simon Deng <simonsdeng@gmail.com>
2014-10-04 03:04:31 -0700

    Add ping detection of login pages

--- background.js
+++ background.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+var interval = 3000;
+
+var handleLoginPage = function (response) {
+	console.log("login");
+};
+
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function () {
+	if (this.response.indexOf("User-agent:")) {
+		handleLoginPage(this.response);
+	} else {
+		console.log("ping");
+	}
+};
+
+var startPing = function () {
+	if (navigator.onLine) {
+		xhr.open("GET", "http://www.google.com/robots.txt");
+		xhr.send();
+	}
+};
+
+xhr.onloadend = function () { setTimeout(startPing, interval) };
+
+startPing();
