d490d0dcb37cf21f3fadaf6672bb6f454401f400
Ravin Sardal <ravin.sardal@gmail.com>
2014-10-04 20:21:06 -0700

    Callback function for autologin added

--- handleLogin.js
+++ handleLogin.js
@@ -1,24 +1,38 @@
-var i, j;
-var form;
-var onsubmit;
+var autologin = function (message) {
 
-var generateHandler = function (form, i, onsubmit) {
-	return function () {
-		if (onsubmit) onsubmit();
-
-		var data = {};
-		var input;
-
-		for (j = 0; j < form.elements.length; j++) {
-			input = form.elements[j]
-			if (input.name) data[input.name] = input.value;
-		}
-
-		chrome.runtime.sendMessage({form: i, data: data});
-	};
 };
 
-for (i = 0; i < document.forms.length; i++) {
-	form = document.forms[i];
-	form.onsubmit = generateHandler(form, i, form.onsubmit);
-}
+var recordData = function () {
+	var i;
+	var form;
+
+	var generateHandler = function (form, i, onsubmit) {
+		return function () {
+			if (onsubmit) onsubmit();
+
+			var data = {};
+			var j;
+			var input;
+
+			for (j = 0; j < form.elements.length; j++) {
+				input = form.elements[j]
+				if (input.name) data[input.name] = input.value;
+			}
+
+			chrome.runtime.sendMessage({form: i, data: data});
+		};
+	};
+
+	for (i = 0; i < document.forms.length; i++) {
+		form = document.forms[i];
+		form.onsubmit = generateHandler(form, i, form.onsubmit);
+	}
+};
+
+chrome.runtime.onMessage.addListener(function (message) {
+	if (false && !message.empty /*or something like this*/) {
+		autologin(message); // or something
+	} else {
+		recordData();
+	}
+});
